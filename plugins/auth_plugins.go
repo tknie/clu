1edbe4afe50e80c32287b183d8f7c873b1c67fb8
Thorsten A. Knieling <thorsten.knieling@googlemail.com>
2024-10-20 21:06:30 +0200

    Add comments warnings

--- plugins/auth_plugins.go
+++ plugins/auth_plugins.go
@@ -17,7 +17,7 @@ import (
 	"github.com/tknie/services/auth"
 )
 
-// HandleBasicAuth handle basic authorisation
+// BasicAuth handle basic authorisation
 func BasicAuth(user string, pass string) (auth.PrincipalInterface, error) {
 	sessionUUID := &auth.SessionInfo{}
 	principal := auth.PrincipalCreater(sessionUUID, user, pass)
@@ -30,6 +30,7 @@ func BasicAuth(user string, pass string) (auth.PrincipalInterface, error) {
 	return nil, fmt.Errorf("no plugins")
 }
 
+// HandleBearerAuth handle bearer auth
 func HandleBearerAuth(token string) (auth.PrincipalInterface, error) {
 	return nil, fmt.Errorf("no plugins")
 }
